1a3637958902ad17c6eeca948b50b55c9f9813b0
Margo Lanier <lanier.margo@gmail.com>
2017-02-01 17:43:53 -0500

    added a business service with GET request, displaying biz stats to dom

--- js/controllers/play-game.js
+++ js/controllers/play-game.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: 'PlayGame',
-	task($scope, $state, CreateStand, Supplies) {
+	task($scope, $state, CreateStand, Supplies, Business) {
 		
 		// Get stand id and set it for all relevent services
 		let stand_id = CreateStand.getId();
@@ -8,10 +8,16 @@ module.exports = {
 			$state.go('home');
 		}
 		Supplies.setId(stand_id);
+		Business.setId(stand_id);
 		
 		// Get current ingredients
 		Supplies.getIngredients().then(function(updatedItems) {
 			$scope.ingredients = updatedItems;
+		});
+		
+		// Get business stats
+		Business.getStats().then(function(updatedStats) {
+			$scope.stats = updatedStats;
 		});
 	},
 };
--- js/services/business.js
+++ js/services/business.js
@@ -1,20 +1,43 @@
 module.exports = {
-	name: 'CreateStand',
+	name: 'Business',
 	
 	task($http) {
 		let stand_id = null;
 		
+		let myBiz = {
+			day: 0,
+			inBiz: true,
+			daysInBiz: 0,
+			balance: 0,
+			cupPrice: 0,
+			prevDayCustomers: 0,
+			prevDayVisitors: 0,
+			totalCustomers: 0,
+			totalVisitors: 0,
+		};
+		
 		return {
-			startGame(bizName) {
-				return $http.post('https://blooming-hamlet-70507.herokuapp.com/stand', {
-					stand_name: bizName,
-				}).then(function(response) {
-					stand_id = response.data.stand_id;
-				});
+			setId(id) {
+				stand_id = id;
 			},
 			
-			getId() {
-				return stand_id;
+			getStats() {
+				return $http.get(`https://blooming-hamlet-70507.herokuapp.com/stand/${stand_id}`).then(function(response) {
+					let stats = response.data;
+					
+					// Update business stats
+					myBiz.day = stats.day;
+					myBiz.inBiz = stats.in_business;
+					myBiz.daysInBiz = stats.day - stats.started_on;
+					myBiz.balance = stats.business.balance;
+					myBiz.cupPrice = stats.business.cupPrice;
+					myBiz.prevDayCustomers = stats.business.yesterday_cups_sold;
+					myBiz.prevDayVisitors = stats.business.yesterday_visitors;
+					myBiz.totalCustomers = stats.business.total_cups_sold;
+					myBiz.totalVisitors = stats.business.total_visitors;
+					
+					return myBiz;
+				});
 			},
 		};
 	},
